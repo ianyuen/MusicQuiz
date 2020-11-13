a4ae214c11236a22a76d59228ed7bb67e185fe25
long.nguyenthanh4 <long.nguyenthanh4@gameloft.com>
2020-11-13 10:58:59 +0700

    Result scene

--- Assets/Scripts/Scenes/Result.cs
+++ Assets/Scripts/Scenes/Result.cs
@@ -1,13 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Result : MonoBehaviour
 {
+    Vector2 scrollViewVector = Vector2.zero;
+
+    Playlist playlist;
+    List<Choice> userChoices;
+
+    public void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+        GUILayout.BeginVertical("Box");
+
+        if (GUILayout.Button("Replay"))
+        {
+            GameManager.Instance.Score = 0;
+            GameManager.Instance.UserChoices = new List<Choice>();
+            SceneManager.LoadScene("Welcome");
+        }
+
+        GUILayout.Label("Score: " + GameManager.Instance.Score.ToString() + "/" + userChoices.Count.ToString());
+
+        scrollViewVector = GUILayout.BeginScrollView(scrollViewVector);
+        for (int index = 0; index < userChoices.Count; index++)
+        {
+            GUILayout.Label("Question: " + (index + 1).ToString());
+
+            Song song = playlist.questions[index].song;
+            GUILayout.Label("Answer: " + song.title);
+
+            Choice choice = userChoices[index];
+            GUILayout.Label("You: " + choice.title);
+
+            GUILayout.Label("");
+        }
+        GUILayout.EndScrollView();
+
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        foreach (Playlist playlist in GameManager.Instance.Playlists)
+        {
+            if (playlist.id == GameManager.Instance.PlaylistID)
+            {
+                this.playlist = playlist;
+            }
+        }
 
+        userChoices = GameManager.Instance.UserChoices;
     }
 
     // Update is called once per frame
